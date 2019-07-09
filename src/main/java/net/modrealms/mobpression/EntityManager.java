e89941c7beda36da6d02acefd83ccd30e7c916d1
Ally <sonically@hotmail.co.uk>
2019-07-09 22:37:15 +0100

    There is now an entity checker that will look for invalid mobs every minute.

--- src/main/java/net/modrealms/mobpression/EntityManager.java
+++ src/main/java/net/modrealms/mobpression/EntityManager.java
@@ -1,18 +1,25 @@
 package net.modrealms.mobpression;
 
 import lombok.Getter;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.world.World;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class EntityManager {
 
     @Getter
-    private HashMap<UUID, Integer> entityMap;
+    public HashMap<UUID, Integer> entityMap;
 
     public EntityManager(){
         this.entityMap = new HashMap<>();
         loadData();
+        /* Run the entitycheck task */
+        Task.builder().interval(1, TimeUnit.MINUTES).execute(new EntityCheck()).submit(Mobpression.getInstance());
     }
 
     public void updateEntity(UUID uuid, int amount){
@@ -30,5 +37,27 @@ public class EntityManager {
         Mobpression.getInstance().getDatabaseHandler().getEntities().forEach((uuid, amount) -> {
             this.entityMap.put(uuid, amount);
         });
+    }
+
+    private class EntityCheck implements Runnable {
+
+        @Override
+        public void run() {
+            EntityManager.this.getEntityMap().forEach((uuid, amount) -> {
+                if(!existsInServer(uuid)){
+                    Mobpression.getInstance().getLogger().severe("Entity no longer exists! Removing it from the map");
+                    EntityManager.this.removeEntity(uuid);
+                }
+            });
+        }
+
+        private boolean existsInServer(UUID uuid){
+            for(World world : Sponge.getServer().getWorlds()){
+                if(world.getEntity(uuid).isPresent()){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
