97f20c30a8325794919e960801f67a210a2e74bb
TheFlash787 <sonically@hotmail.co.uk>
2019-07-20 21:15:19 +0100

    Iterate over the entries in the map to remove entities

--- src/main/java/net/modrealms/mobpression/EntityManager.java
+++ src/main/java/net/modrealms/mobpression/EntityManager.java
@@ -6,10 +6,7 @@ import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.world.World;
 
 import javax.swing.text.html.parser.Entity;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class EntityManager {
@@ -21,7 +18,7 @@ public class EntityManager {
         this.entityMap = new HashMap<>();
         loadData();
         /* Run the entitycheck task */
-//        Task.builder().execute(new EntityCheck()).interval(1, TimeUnit.MINUTES).delay(5, TimeUnit.SECONDS).async().submit(Mobpression.getInstance());
+//        Task.builder().execute(new EntityCheck()).interval(1, TimeUnit.MINUTES).delay(5, TimeUnit.SECONDS).submit(Mobpression.getInstance());
     }
 
     public void updateEntity(UUID uuid, int amount){
@@ -45,15 +42,10 @@ public class EntityManager {
 
         @Override
         public void run() {
-            List<UUID> invalidEntities = new ArrayList<>();
-            Mobpression.getInstance().getEntityManager().getEntityMap().forEach((uuid, amount) -> {
-                if(!existsInServer(uuid)){
-                    Mobpression.getInstance().getLogger().severe("Entity no longer exists! Removing it from the map");
-                    invalidEntities.add(uuid);
+            for(Map.Entry<UUID, Integer> entry : Mobpression.getInstance().getEntityManager().getEntityMap().entrySet()){
+                if(!existsInServer(entry.getKey())){
+                    Mobpression.getInstance().getEntityManager().removeEntity(entry.getKey());
                 }
-            });
-            for(UUID uuid : invalidEntities){
-                Mobpression.getInstance().getEntityManager().removeEntity(uuid);
             }
         }
 
